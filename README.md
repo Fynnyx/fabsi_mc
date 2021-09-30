0258b73788c3f374cc56eeb37adb69da31854ca3
Fynnyx <westerathf@gmail.com>
2021-09-30 19:33:50 +0200

    new command added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# fabsi_mc
+# Fabsi_MC
+
+## Discord
+Discord bot to handle the discord from Fabsi
+
+## Twitch
+Twitch bot to handle the Chat and add own commands and personalize it to 100%.
--- twitch/fabsichatbot.py
+++ twitch/fabsichatbot.py
@@ -3,9 +3,11 @@ from twitchio.ext import commands
 from twitchio.ext import routines
 import datetime
 import random
+import requests
 import json
 
 with open("properties.json", encoding='UTF-8') as f:
+    global data
     data = json.load(f)
 
 bot = commands.Bot(
@@ -18,32 +20,47 @@ bot = commands.Bot(
 @bot.event()
 async def event_ready():
     print("FabsiChatBot: logged in")
+
     # global variables
     global isInfoEnabled
-    isInfoEnabled = True
+    isInfoEnabled = data["properties"]["routines"]["information"]["default"]
+
     # Start routines
     info_rout.start()
 
 
 # Routines ---------------------------------------------------------------------------
 
+@routines.routine(seconds=data["properties"]["routines"]["update_data"]["time"])
+async def update_json(ctx):
+    with open("properties.json", encoding='UTF-8') as f:
+        global data
+        data = json.load(f)
+
 @bot.command(aliases=data["properties"]["commands"]["enable_info"])
-async def enable_info():
-    isInfoEnabled = True
+async def enable_info(ctx):
+    if ctx.author.is_mod:
+        global isInfoEnabled 
+        isInfoEnabled = True
 
 @bot.command(aliases=data["properties"]["commands"]["disable_info"])
-async def disable_info():
-    isInfoEnabled = False
+async def disable_info(ctx):
+    if ctx.author.is_mod:
+        global isInfoEnabled 
+        isInfoEnabled = False
 
 
 @routines.routine(seconds=data["properties"]["routines"]["information"]["time"])
 async def info_rout():
-    if isInfoEnabled:
+    if isInfoEnabled == True:
         messages = data["properties"]["routines"]["information"]["messages"]
         index = random.randint(0, len(messages)-1)
         # print(messages[index])
-        channel = bot.get_channel("Fabsi_MC")
+        channel = bot.get_channel(data["properties"]["channel"])
         await channel.send(messages[index])
+
+
+# Commands ---------------------------------------------------------------------------
 
 
 # @bot.event()
@@ -52,7 +69,21 @@ async def info_rout():
     # print(ctx.content)
 
 @bot.command()
-async def dc(ctx: commands.Context):
+async def test(ctx):
+    print("Test"),
+    print(ctx.author),
+    print(ctx.channel)
+
+@bot.command()
+async def dc(ctx):
     await ctx.send("Fabsi's Discord: %s" % (data["properties"]["social_media"]["discord"]))
+
+# @bot.command()
+# async def followage(ctx, name):
+#     if name:
+        
+#     else:
+#         r = requests.get('https://2g.be/twitch/following.php?user=%s&channel=%s&format=mwdhms' % (ctx.author.name, ctx.channel.name)) 
+#     await ctx.send(r.text)
 
 bot.run()
